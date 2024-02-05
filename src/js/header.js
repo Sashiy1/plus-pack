8ecff8db38aaa8095d6381503c97175dd48c49b5
Sashiy1 <aleksander.paluch@wp.pl>
2024-02-05 10:43:58 +0100

    video added

--- src/js/header.js
+++ src/js/header.js
@@ -1,10 +1,10 @@
 const burgerButton = document.querySelector('.menu')
 const burgerMenu = document.querySelector('.burger-menu')
-
+const firstContainer = document.querySelector('.first-container')
 burgerButton.addEventListener("click", handleClick)
-
+console.log(firstContainer)
 
 function handleClick (event) {
     burgerMenu.classList.toggle('showMenu')
-    
+    firstContainer.classList.toggle('moveContainer')
 }
